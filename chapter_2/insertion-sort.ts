61ea77f9ce6dfc2b68c8718cf926b42dbed92c84
Joel Myers <joeldmyers@gmail.com>
2022-04-11 20:04:15 -0400

    adding solutions to problems

--- chapter_2/insertion-sort.ts
+++ chapter_2/insertion-sort.ts
@@ -10,7 +10,8 @@ export const insertionSort = (numbers: number[]): number[] => {
     pointerDividingSortedFromUnsorted++
   ) {
     // insert currentNum into the sorted sequence to the left
-    const currentNumber = numbers[pointerDividingSortedFromUnsorted];
+    const currentlySelectedNumberToSort =
+      numbers[pointerDividingSortedFromUnsorted];
 
     // let's look one to the left of the current number
     let indexOfCurrentSortedItemWeAreLookingAt =
@@ -18,7 +19,8 @@ export const insertionSort = (numbers: number[]): number[] => {
 
     while (
       indexOfCurrentSortedItemWeAreLookingAt >= 0 &&
-      currentNumber <= numbers[indexOfCurrentSortedItemWeAreLookingAt]
+      currentlySelectedNumberToSort <=
+        numbers[indexOfCurrentSortedItemWeAreLookingAt]
     ) {
       /**
        * this condition starts from just to the left of the pointer dividing the sorted from the unsorted.
@@ -33,10 +35,71 @@ export const insertionSort = (numbers: number[]): number[] => {
       numbers[indexOfCurrentSortedItemWeAreLookingAt + 1] =
         numbers[indexOfCurrentSortedItemWeAreLookingAt];
       indexOfCurrentSortedItemWeAreLookingAt--;
-      numbers[indexOfCurrentSortedItemWeAreLookingAt + 1] = currentNumber;
+      numbers[indexOfCurrentSortedItemWeAreLookingAt + 1] =
+        currentlySelectedNumberToSort;
     }
   }
 
   // this should now be sorted.
   return numbers;
+};
+
+/**
+ * Insertion Sort sorts in place (decreasing - exercise 2.1-2)
+ */
+export const insertionSortDecreasing = (numbers: number[]): number[] => {
+  // skip numbers[0] since this is "sorted"; start at index 1.
+
+  for (
+    let pointerDividingSortedFromUnsorted = 1;
+    pointerDividingSortedFromUnsorted < numbers.length;
+    pointerDividingSortedFromUnsorted++
+  ) {
+    // insert currentNum into the sorted sequence to the left
+    const currentlySelectedNumberToSort =
+      numbers[pointerDividingSortedFromUnsorted];
+
+    // let's look one to the left of the current number
+    let indexOfCurrentSortedItemWeAreLookingAt =
+      pointerDividingSortedFromUnsorted - 1;
+
+    while (
+      indexOfCurrentSortedItemWeAreLookingAt >= 0 &&
+      currentlySelectedNumberToSort >=
+        numbers[indexOfCurrentSortedItemWeAreLookingAt]
+    ) {
+      /**
+       * this condition starts from just to the left of the pointer dividing the sorted from the unsorted.
+       * It only continues (does anything) if
+       * the number in the sorted array we are looking at is greater than
+       * the number we are pulling over from the unsorted numbers
+       * if the current number is greater than or equal to the sorted number we are examining,
+       * we leave it where it is (to the right)
+       * if it's less than, we swap it with the one to the left of it and move over one and go again.
+       */
+
+      numbers[indexOfCurrentSortedItemWeAreLookingAt + 1] =
+        numbers[indexOfCurrentSortedItemWeAreLookingAt];
+      indexOfCurrentSortedItemWeAreLookingAt--;
+      numbers[indexOfCurrentSortedItemWeAreLookingAt + 1] =
+        currentlySelectedNumberToSort;
+    }
+  }
+
+  // this should now be sorted.
+  return numbers;
+};
+
+// exercise 2.1-3
+export const linearSearch = (
+  numbers: number[],
+  target: number
+): number | null => {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === target) {
+      return i;
+    }
+  }
+
+  return null;
 };
--- chapter_2/tests/insertion-sort.test.ts
+++ chapter_2/tests/insertion-sort.test.ts
@@ -1,4 +1,8 @@
-import { insertionSort } from "../insertion-sort";
+import {
+  insertionSort,
+  insertionSortDecreasing,
+  linearSearch,
+} from "../insertion-sort";
 
 test("insertion sort", () => {
   const unsortedNums = [5, 2, 4, 3, 1];
@@ -10,4 +14,17 @@ test("insertion sort", () => {
   const unsortedNums = [5, 2, 4, 6, 1, 3];
 
   expect(insertionSort(unsortedNums)).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("insertion sort decreasing order", () => {
+  const unsortedNums = [5, 2, 4, 3, 1];
+
+  expect(insertionSortDecreasing(unsortedNums)).toEqual([5, 4, 3, 2, 1]);
+});
+
+test("linear search", () => {
+  const arrayToSearch = [6, 3, 9, 12, 15, 21];
+  const target = 21;
+
+  expect(linearSearch(arrayToSearch, target)).toEqual(5);
 });
