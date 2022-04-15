6caee21991f868c3f6b3298a30f69fe830734a8c
Joel Myers <joeldmyers@gmail.com>
2022-04-14 22:17:19 -0400

    merge sort

---
+++ chapter_2/merge-sort.ts
@@ -0,0 +1,51 @@
+export const mergeSort = (nums: number[]): number[] => {
+  // base case
+  if (nums.length === 1) {
+    return nums;
+  }
+
+  // divide & conquer
+  const midIndex = Math.floor(nums.length / 2);
+
+  const leftNums = mergeSort(nums.slice(0, midIndex));
+
+  const rightNums = mergeSort(nums.slice(midIndex, nums.length));
+
+  // merge - need to write a merge function
+
+  return mergeSortedArrays(leftNums, rightNums);
+};
+
+const mergeSortedArrays = (
+  sortedArray1: number[],
+  sortedArray2: number[]
+): number[] => {
+  let newlySortedArray = [];
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  while (pointer1 < sortedArray1.length || pointer2 < sortedArray2.length) {
+    const currentItem1 = sortedArray1[pointer1];
+    const currentItem2 = sortedArray2[pointer2];
+    if (!currentItem1) {
+      newlySortedArray.push(currentItem2);
+      pointer2++;
+    }
+
+    if (!currentItem2) {
+      newlySortedArray.push(currentItem1);
+      pointer1++;
+    }
+
+    if (sortedArray1[pointer1] <= sortedArray2[pointer2]) {
+      newlySortedArray.push(sortedArray1[pointer1]);
+      pointer1++;
+    }
+
+    if (sortedArray2[pointer2] < sortedArray1[pointer1]) {
+      newlySortedArray.push(sortedArray2[pointer2]);
+      pointer2++;
+    }
+  }
+  return newlySortedArray;
+};
---
+++ chapter_2/tests/merge-sort.test.ts
@@ -0,0 +1,7 @@
+import { mergeSort } from "../merge-sort";
+
+test("Selection sort", () => {
+  const unsortedNums = [5, 2, 4, 6, 1, 3];
+
+  expect(mergeSort(unsortedNums)).toEqual([1, 2, 3, 4, 5, 6]);
+});
