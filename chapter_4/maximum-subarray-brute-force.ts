cc20cbd46cdce5cef6deabf306958749616717d6
Joel Myers <joeldmyers@gmail.com>
2022-05-16 18:31:18 -0400

    brute force version

---
+++ chapter_4/maximum-subarray-brute-force.ts
@@ -0,0 +1,21 @@
+import { MaxSubarrayResult } from "./maximum-subarray";
+
+export const maxSubarrayBruteForce = (nums: number[]): MaxSubarrayResult => {
+  let maxSum = Number.NEGATIVE_INFINITY;
+  let winningLeftIndex, winningRightIndex;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum > maxSum) {
+        maxSum = sum;
+        winningLeftIndex = i;
+        winningRightIndex = j;
+      }
+    }
+  }
+
+  return { low: winningLeftIndex, high: winningRightIndex, sum: maxSum };
+};
--- chapter_4/tests/maximum-subarray.test.ts
+++ chapter_4/tests/maximum-subarray.test.ts
@@ -1,4 +1,5 @@
 import { findMaximumSubarray } from "../maximum-subarray";
+import { maxSubarrayBruteForce } from "../maximum-subarray-brute-force";
 
 it("finds the maximum subarray", () => {
   const priceChangeData = [
@@ -10,4 +11,14 @@ it("finds the maximum subarray", () => {
   expect(
     findMaximumSubarray(priceChangeData, 0, priceChangeData.length - 1)
   ).toEqual(expectedResult);
+});
+
+it("finds the maximum subarray using brute force", () => {
+  const priceChangeData = [
+    13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
+  ];
+
+  const expectedResult = { low: 7, high: 10, sum: 43 };
+
+  expect(maxSubarrayBruteForce(priceChangeData)).toEqual(expectedResult);
 });
