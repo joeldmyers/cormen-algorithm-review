47f3b07fe7e3e3ef8545c716d08317c81a806951
Joel Myers <joeldmyers@gmail.com>
2022-06-11 06:34:52 -0400

    wip heap methods

--- chapter_6/CHAPTER_6_NOTES.md
+++ chapter_6/CHAPTER_6_NOTES.md
@@ -1,3 +1,13 @@
 # Sorting
 
 ![IMG_2204](https://user-images.githubusercontent.com/4838984/172373266-021ffd50-b5e4-4279-9ce1-4d7632fbc202.jpg)
+
+## Heapsort
+
+### Heaps
+
+A heap is an array object that we can view as a nearly complete binary tree. Each node of the tree corresponds to an element of the array. The tree is completely filled on all levels except possibly the lowest, which is filled from the left up to a point. The root of the tree is the first element of the tree, and given the index i of a node, we can easily compute the indices of its parent, left child, and right child.
+
+Parent of i is i / 2, left child of i is 2i, right child of i is 2i + 1.
+
+A max heap is a binary tree with numbers going down from high to low (>=); so root would be say 16, children would be 14 and 12, etc. Min heap is the opposite.
---
+++ chapter_6/max-heap-methods.ts
@@ -0,0 +1,46 @@
+const getLeftChildIndex = (i: number) => 2 * i;
+
+const getRightChildIndex = (i: number) => 2 * i + 1;
+
+/**
+ * This runs in O log n time (also O of h) and is the key to maintaining the max heap property.
+ * This lets the value at array[i] "float down" if it's less than its children, to
+ * make sure that it obeys the rules of max heap.
+ * p. 154.
+ */
+export const maxHeapify = (array: number[], i: number) => {
+  const leftChildIndex = getLeftChildIndex(i);
+  const rightChildIndex = getRightChildIndex(i);
+
+  let largest;
+
+  if (leftChildIndex <= array.length && array[leftChildIndex] > array[i]) {
+    largest = leftChildIndex;
+  } else {
+    largest = i;
+  }
+
+  if (rightChildIndex <= array.length && array[rightChildIndex] > array[i]) {
+    largest = rightChildIndex;
+  }
+
+  if (largest !== i) {
+    // exchange array[i] with array[largest].
+    const temp = array[i];
+    array[i] = array[largest];
+    array[largest] = temp;
+
+    maxHeapify(array, largest);
+  }
+};
+
+/**
+ * All the elements in the subarray A[(n/2+1)] are all leaves of the tree, so each is a 1-element heap to begin with.
+ * So we start at the middle and work back to the beginning.
+ */
+
+export const buildMaxHeap = (array: number[]) => {
+  for (let i = array.length / 2; i >= 0; i--) {
+    maxHeapify(array, i);
+  }
+};
