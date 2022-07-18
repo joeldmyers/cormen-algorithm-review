b474df1ebaf0f34c959db5925f3bd8d7d35cc4a9
Joel Myers <joeldmyers@gmail.com>
2022-07-18 10:31:26 +0200

    deleting practice folder

--- practice/merge-sort/tests/merge-sort.test.ts
+++
@@ -1,7 +0,0 @@
-import { mergeSort } from "../merge-sort";
-
-test("merge sort", () => {
-  const unsortedNums = [5, 2, 4, 6, 1, 3];
-
-  expect(mergeSort(unsortedNums)).toEqual([1, 2, 3, 4, 5, 6]);
-});
