a3467b4d8075c3546f463cacdcb09d14a74de23c
Joel Myers <joeldmyers@gmail.com>
2022-08-29 20:13:38 -0400

    iterative activity selector solution

---
+++ chapter_16/activity-selection.js
@@ -0,0 +1,26 @@
+/**
+ * The problem here is say we have a set of n proposed activities that want to use a resource,
+ * e.g., a lecture hall, which can only serve one activity at a time. Each activity
+ * has a start time and an end time.
+ * start and end times are compatible if they do not overlap.
+ *
+ * We want to select a maximum-size subset of mutually compatible activities.
+ */
+/**
+ *
+ * @param {number[]} startTimes
+ * @param {number[]} finishTimes
+ */
+const greedyActivitySelector = (startTimes, finishTimes) => {
+  // start with the first one.
+  const result = [0];
+  let indexOfMostRecentAdditionToA = 0;
+
+  for (let i = 1; i < s.length; i++) {
+    if (startTimes[i] >= finishTimes[indexOfMostRecentAdditionToA]) {
+      result.push(i);
+      indexOfMostRecentAdditionToA = i;
+    }
+  }
+  return result;
+};
